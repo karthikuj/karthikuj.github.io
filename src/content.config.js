cfea072135bdbeeeb63e05fd0dd6e27a25533bdf
Karthik UJ <karthikuj2001@gmail.com>
2026-06-06 21:58:02 +0530

    adds first blog about securing sections
    Signed-off-by: Karthik UJ <karthikuj2001@gmail.com>

--- src/content.config.js
+++ src/content.config.js
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-	loader: glob({ pattern: '*.md', base: './src/data/blog-posts' }),
+	loader: glob({ pattern: '**/*.md', base: './src/data/blog-posts' }),
 	schema: z.object({
 		title: z.string(),
 		slug: z.string(),
